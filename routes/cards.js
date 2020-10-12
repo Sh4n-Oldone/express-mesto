eed0e6bd71bc1300838679c08a9c2fad5760c62c
Alex Pavlov <halfer.orc@gmail.com>
2020-10-13 00:50:49 +0300

    fix(): json parse

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,22 @@
-const router = require('express').Router();
 const cardsRouter = require('express').Router();
-const cards = require('../data/cards.json');
+const path = require('path');
+const fs = require('fs');
+
+const dataPath = path.join(__dirname, '..', 'data', 'cards.json');
+const cardsData = fs.readFileSync(dataPath, { encoding: 'utf8' });
 
 cardsRouter.get('/', (req, res) => {
-  res.status(200).send(cards);
+  res.status(200).send(JSON.parse(cardsData));
+});
+
+cardsRouter.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const reqCard = JSON.parse(cardsData).find(card => card._id === id)
+  if (reqCard) {
+    res.status(200).send(JSON.parse(cardsData));
+  } else {
+    res.status(404).json({"message": "Нет такой карточки"});
+  }
 });
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,23 @@
 const usersRouter = require('express').Router();
-const users = require('../data/users.json');
+const path = require('path');
+const fs = require('fs');
+
+const dataPath = path.join(__dirname, '..', 'data', 'users.json');
+const usersData = fs.readFileSync(dataPath, { encoding: 'utf8' });
 
 usersRouter.get('/', (req, res) => {
-  res.status(200).json(users);
-  console.log(`Отдал users: ${users}`);
+  res.status(200).send(JSON.parse(usersData));
+  console.log(`Отдал users: ${usersData}`);
 });
 
 usersRouter.get('/:id', (req, res) => {
   const { id } = req.params;
-  const reqUser = users.find(user => user._id === id)
+  const reqUser = JSON.parse(usersData).find(user => user._id === id)
   if (reqUser) {
     res.status(200).send(reqUser);
   } else {
     res.status(404).json({"message": "Нет пользователя с таким id"});
   }
 });
-
-// usersRouter.get('/', (req, res) => {
-//   const dataPath = path.join(__dirname, 'data', 'users.json');
-//   const usersData = fs.readFile(dataPath, { encoding: 'utf8' });
-//   res.status(200).json(usersData);
-//   console.log(`Отдал users: ${users}`);
-// });
-
-// usersRouter.get('/:id', (req, res) => {
-//   const dataPath = path.join(__dirname, 'data', 'users.json');
-//   const usersData = fs.readFile(dataPath, { encoding: 'utf8' });
-//   const { id } = req.params;
-//   const reqUser = usersData.find(user => user._id === id);
-//   if (reqUser) {
-//     res.status(200).send(reqUser);
-//   } else {
-//     res.status(404).json({"message": "Нет пользователя с таким id"});
-//   }
-// });
 
 module.exports = usersRouter;

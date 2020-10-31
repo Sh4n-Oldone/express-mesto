fffca578b59ca929734166733e86348d846376f5
Alex Pavlov <halfer.orc@gmail.com>
2020-10-31 03:04:35 +0300

    feat(routes/users): обновить профиль и аватар

--- controllers/users.js
+++ controllers/users.js
@@ -50,3 +50,47 @@ module.exports.createUser = (req, res) => {
       return res.status(500).send({ message: 'Ошибка сервера при создании пользователя' });
     });
 };
+
+module.exports.updateUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const updatingBody = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      userId, updatingBody, { new: true, runValidators: true },
+    );
+    if (!updatedUser) {
+      return res.status(404).send({ message: `Пользователь ${userId} не обнаружен` });
+    }
+    return res.status(200).send({ message: `Профиль обновлён ${updatedUser}` });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Ошибка ввода данных' });
+    }
+    if (err.name === 'CastError') {
+      return res.status(400).send({ message: 'Ошибка ввода' });
+    }
+    return res.status(500).send({ message: 'Ошибка сервера при создании пользователя' });
+  }
+};
+
+module.exports.updateUserAvatar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { avatar } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      userId, { avatar }, { new: true, runValidators: true },
+    );
+    if (!updatedUser) {
+      return res.status(404).send({ message: `Пользователь ${userId} не обнаружен` });
+    }
+    return res.status(200).send({ message: `Аватар обновлён ${updatedUser}` });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Ошибка ввода данных' });
+    }
+    if (err.name === 'CastError') {
+      return res.status(400).send({ message: 'Ошибка ввода' });
+    }
+    return res.status(500).send({ message: 'Ошибка сервера при создании пользователя' });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const usersRouter = require('express').Router();
 
-const { getUsers, getUser, createUser } = require('../controllers/users');
+const {
+  getUsers, getUser, createUser, updateUser, updateUserAvatar,
+} = require('../controllers/users');
 
 usersRouter.get('/users', getUsers);
 usersRouter.get('/users/:userId', getUser);
 usersRouter.post('/users', createUser);
+usersRouter.patch('/users/me', updateUser);
+usersRouter.patch('/users/me/avatar', updateUserAvatar);
 
 module.exports = usersRouter;

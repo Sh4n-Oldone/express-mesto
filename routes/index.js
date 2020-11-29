2ee08de40f21d25e6faad4d4836de8fde5b3240e
Alex Pavlov <halfer.orc@gmail.com>
2020-11-29 16:59:43 +0300

    fix index middleware

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const router = require('express').Router();
 
 const usersRouter = require('./users.js');
 const cardsRouter = require('./cards.js');
+const NotFoundError = require('../errors/notFoundError.js');
 
 router.use(
   usersRouter,
   cardsRouter,
+  (req, res) => {
+    throw new NotFoundError('запрашиваемый ресурс не найден');
+  },
 );
 
 module.exports = router;

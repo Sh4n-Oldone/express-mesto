68907e895da99b6c794fc38f7ce7536fcd9b10fc
Alex Pavlov <halfer.orc@gmail.com>
2020-11-29 03:10:28 +0300

    test3 celeb userme

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,11 @@ const {
 
 usersRouter.get('/users', getUsers);
 usersRouter.get('/users/me', celebrate({
-  user: Joi.object(),
+  body: Joi.object.keys({
+    user: Joi.object().keys({
+      _id: Joi.string().hex().max(24),
+    }),
+  }),
 }), getMe);
 usersRouter.get('/users/:userId', getUser);
 usersRouter.patch('/users/me', celebrate({

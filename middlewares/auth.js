88174317ece88cb7983e93d38f8dd072115d043e
Alex Pavlov <halfer.orc@gmail.com>
2020-12-04 22:03:15 +0300

    fix(auth) if or, not and

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ const { JWT_SECRET = 'dev-secret-key' } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization && !authorization.startsWith('Bearer ')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new NotAuthorizeError('Необходима авторизация');
   }
 
